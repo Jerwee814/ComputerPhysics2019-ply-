123dfbf88631fdd5ba311eba96c6b3a63c6729ca
Jerwee814 <52267184+Jerwee814@users.noreply.github.com>
2019-11-09 15:01:57 +0800

    Add files via upload

---
+++ test_function.h
@@ -0,0 +1,13 @@
+#include <cmath>
+#include <iostream>
+using namespace std;
+
+//f(x)=x^6
+double power_6(double x) {
+	return (x * x*x*x*x*x);
+}
+//f(x)=x^7
+double power_5(double x) {
+	return (x * x*x*x*x);
+}
+//f(x)=??
